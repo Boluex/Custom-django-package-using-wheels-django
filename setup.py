cf7d815d471193d303e13ee73ebfd482b4e3ad9e
Boluex <awomewekolawole1@gmail.com>
2024-04-01 19:15:13 +0100

    Another commit

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import find_packages,setup
-with open('login_proj/readme.MD') as f:
-    long_des=f.read()
+import os
+# with open('readme.MD') as f:
+#     long_des=f.read()
+
+base_dir = os.path.abspath(os.path.dirname('login_proj'))
+# base_dir=os.listdir('')
+
+# Read the contents of the readme file
+with open(os.path.join(base_dir, 'readme.MD'), encoding='utf-8') as f:
+    long_des = f.read()
 
 setup(
     name='Dj_login_auth',
     version='0.0.1',
     description='A reusable django login auth application',
-    package_dir= {'','login_proj'},
     packages= find_packages(where='login_proj'),
     long_description= long_des,
     long_description_content_type='text/markdown',
@@ -18,7 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Code Generators', 
         'Programming Language :: Python :: 3.10',
-        'operating system  :: os independent',
+        'Operating System :: OS Independent',
     ],
     install_requires=[],
     keywords='django reusable app',
